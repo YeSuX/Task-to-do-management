028de19cb515be17fa50cc1bf7795c32f812f207
YeSuX <351490949@qq.com>
2021-08-13 14:36:54 +0800

    start: 项目初始化

---
+++ src/views/NavigationDrawerItems/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title class="text-h6"> 任务待办 </v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-divider></v-divider>
+
+    <v-list dense nav>
+      <v-list-item v-for="item in items" :key="item.title" link>
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavigationDrawerItems",
+  data() {
+    return {
+      items: [
+        { title: "Dashboard", icon: "mdi-view-dashboard" },
+        { title: "Photos", icon: "mdi-image" },
+        { title: "About", icon: "mdi-help-box" },
+      ],
+      right: null,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vue.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+module.exports = {
+  publicPath: '/',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  lintOnSave: false,
+  productionSourceMap: false,
+  devServer: {
+    port: 8080,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true
+    },
+  },
+  configureWebpack: {
+    // provide the app's title in webpack's name field, so that
+    // it can be accessed in index.html to inject the correct title.
+    name: 'codepen',
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    }
+  },
+  transpileDependencies: [
+    'vuetify',
+  ],
+};
